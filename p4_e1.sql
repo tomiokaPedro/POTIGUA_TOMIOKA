3e045196337d55941d27fc5610938e21e9314b65
TomiokaPedro <pedro.tomioka@gmail.com>
2014-09-11 19:54:23 -0300

    formatado

--- p4_e1.sql
+++ p4_e1.sql
@@ -1,18 +1,15 @@
-create or replace view vw_candidato (nome, cpf, apelido, total_candidaturas) as SELECT
-DISTINCT
-  candidato.nome,
-  candidato.cpf,
-  candidato.apelido,
-  COUNT(apelido) OVER (PARTITION BY nome) AS total_candidaturas
-FROM candidato
-INNER JOIN candidatura
+CREATE OR REPLACE VIEW vw_candidato (nome, cpf, apelido, total_candidaturas)
+AS
+  SELECT DISTINCT candidato.nome,
+    candidato.cpf,
+    candidato.apelido,
+    COUNT(1) OVER (PARTITION BY nome) AS total_candidaturas
+  FROM candidato
+  INNER JOIN candidatura
   ON candidato.nrocand = candidatura.nrocand
-WHERE (SELECT
-  COUNT(1)
-FROM candidatura can
-WHERE can.reg <> candidatura.reg
-AND can.nrocand = candidato.nrocand)
-> 0
-;
-
-select * from vw_candidato;
+  WHERE (SELECT COUNT(1)
+    FROM candidatura can
+    WHERE can.reg  <> candidatura.reg
+    AND can.nrocand = candidato.nrocand) > 0 ;
+    
+  SELECT * FROM vw_candidato;
