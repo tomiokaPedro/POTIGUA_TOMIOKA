affce9ac7eb0ddbbed301a1a5450f18f6097f077
TomiokaPedro <pedro.tomioka@gmail.com>
2014-09-18 20:26:50 -0300

    corrigindo a questao

--- Pratica_5/p5_e2.sql
+++ Pratica_5/p5_e2.sql
@@ -1,6 +1,10 @@
+DECLARE
+  v_ano_atual NUMBER;
 BEGIN
+  --v_ano_atual := to_number(to_char(sysdate,'yyyy'));
+  v_ano_atual := 2004;
   FOR c_cargo IN
-  (SELECT L10_CARGO.CODCARGO,
+  (SELECT DISTINCT L10_CARGO.CODCARGO,
     L10_CARGO.NOMEDESCRITIVO,
     L10_CARGO.ANOSMANDATO,
     L10_CARGO.NRODECADEIRAS,
@@ -11,16 +15,16 @@ BEGIN
   ON L10_CARGO.CODCARGO = L11_CANDIDATURA.CODCARGO
   INNER JOIN L09_CANDIDATO
   ON L11_CANDIDATURA.NROCAND = L09_CANDIDATO.NROCAND
-  where L10_CARGO.anobase = '2012'
+  WHERE L10_CARGO.anobase    = v_ano_atual
+  ORDER BY L10_CARGO.CODCARGO,
+    L10_CARGO.NOMEDESCRITIVO
   )
   LOOP
     dbms_output.put ('Cargo ' || c_cargo.codcargo || ', ' || c_cargo.nomedescritivo ||', ' || c_cargo.anosmandato || ' anos de mandato, ' || c_cargo.nrodecadeiras || ' cadeira.');
-    IF c_cargo.possuivice = 1
-    then
-      dbms_output.put_line (' Possui vice: Vice-' || c_cargo.nomedescritivo || '. Em 2012, ' || c_cargo.qtd || ' candidatos concorrendo ao cargo.');
-      
-      else
-    dbms_output.put_line ('Nao possui vice.');
+    IF c_cargo.possuivice = 1 THEN
+      dbms_output.put_line (' Possui vice: Vice-' || c_cargo.nomedescritivo || '. Em '|| v_ano_atual ||', ' || c_cargo.qtd || ' candidatos concorrendo ao cargo.');
+    ELSE
+      dbms_output.put_line ('Nao possui vice.'|| 'Em '|| v_ano_atual ||', ' || c_cargo.qtd || ' candidatos concorrendo ao cargo.');
     END IF;
   END LOOP;
 END;
