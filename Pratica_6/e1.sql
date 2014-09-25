f5db8f676164b2bb2391a304702997625fb3731b
tomiokaPedro <pedro.tomioka@gmail.com>
2014-09-25 18:00:30 -0300

    ex1_quase pronto

--- Pratica_6/e1.sql
+++ Pratica_6/e1.sql
@@ -1,4 +1,36 @@
-create or replace procedure sp_insere_valores(tabela in varchar2,
-nome_atributo in varchar2,
-valor_atributo_novo in varchar2,
-valor_atributo_chave in varchar2)
+CREATE OR REPLACE PROCEDURE SP_INSERE_VALORES(
+    P_TABELA               IN VARCHAR2,
+    P_NOME_ATRIBUTO        IN VARCHAR2,
+    P_VALOR_ATRIBUTO_NOVO  IN VARCHAR2,
+    P_VALOR_ATRIBUTO_CHAVE IN VARCHAR2)
+AS
+  v_pk                NUMBER;
+  v_flag              NUMBER DEFAULT 0;
+  E_PK_NAO_SIMPLES    EXCEPTION;
+  E_TABELA_NAO_EXISTE EXCEPTION;
+BEGIN
+  SELECT COUNT(1)
+  INTO v_flag
+  FROM USER_IND_COLUMNS
+  WHERE TABLE_NAME = P_TABELA;
+  IF v_flag        > 0 THEN
+    RAISE E_TABELA_NAO_EXISTE;
+  END IF;
+  SELECT COUNT(1)
+  INTO v_flag
+  FROM USER_CONSTRAINTS US
+  WHERE CONSTRAINT_TYPE = 'U' AND US.TABLE_NAME = P_TABELA;
+  IF v_flag             > 0 THEN
+    RAISE E_PK_NAO_SIMPLES;
+  ELSE
+    UPDATE TESTE
+    SET p_nome_atributo = P_VALOR_ATRIBUTO_NOVO;
+  END IF;
+EXCEPTION
+WHEN E_PK_NAO_SIMPLES THEN
+  dbms_output.put_line('E_PK_NAO_SIMPLES');
+WHEN E_TABELA_NAO_EXISTE THEN
+  dbms_output.put_line('E_TABELA_NAO_EXISTE');
+WHEN OTHERS THEN
+  dbms_output.put_line(SQLERRM || ' ERRO grave');
+END;
