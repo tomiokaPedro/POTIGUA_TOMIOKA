211f72a99699bf2f4792f554232ad1361b5998d4
tomiokaPedro <pedro.tomioka@gmail.com>
2014-10-07 15:19:12 -0300

    corrigindo algumas linhas em p6_e1

--- Pratica_6/e1.sql
+++ Pratica_6/e1.sql
@@ -8,24 +8,34 @@ AS
   v_flag              NUMBER DEFAULT 0;
   E_PK_NAO_SIMPLES    EXCEPTION;
   E_TABELA_NAO_EXISTE EXCEPTION;
+  sql_stmt            varchar2(2000);
 BEGIN
   SELECT COUNT(1)
   INTO v_flag
   FROM USER_IND_COLUMNS
-  WHERE TABLE_NAME = P_TABELA;
+  WHERE TABLE_NAME = upper(P_TABELA);
+  
   IF v_flag        = 0 THEN
     RAISE E_TABELA_NAO_EXISTE;
   END IF;
+  
   SELECT COUNT(1)
   INTO v_flag
   FROM USER_CONSTRAINTS US
-  WHERE CONSTRAINT_TYPE = 'U' AND US.TABLE_NAME = P_TABELA;
-  IF v_flag             > 0 THEN
+  WHERE CONSTRAINT_TYPE = 'U' AND US.TABLE_NAME = upper(P_TABELA);
+  
+  sql_stmt := 'update '|| p_tabela || ' set ' || p_nome_atributo || '='|| P_VALOR_ATRIBUTO_NOVO ;
+  
+  IF v_flag = 0 THEN
     RAISE E_PK_NAO_SIMPLES;
   ELSE
-    UPDATE TESTE
+    UPDATE p_tabela
     SET p_nome_atributo = P_VALOR_ATRIBUTO_NOVO;
   END IF;
+  
+  
+  
+  
 EXCEPTION
 WHEN E_PK_NAO_SIMPLES THEN
   dbms_output.put_line('E_PK_NAO_SIMPLES');
