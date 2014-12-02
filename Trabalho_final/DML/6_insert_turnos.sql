ae37c6b6d89d1e15c964ea1906deb24f8b82da67
Pedro Tomioka <pedro.tomioka@gmail.com>
2014-12-01 23:47:33 -0200

    Corrigindo o numero do turno
    pra nao chocar com a sequence

--- Trabalho_final/DML/6_insert_turnos.sql
+++ Trabalho_final/DML/6_insert_turnos.sql
@@ -6,5 +6,5 @@ Turno
 
 
 
-insert into turno(descricao_turno) values ('turno');
-insert into turno(descricao_turno) values ('returno');
+insert into turno(descricao_turno,numero_turno) values ('turno',1);
+insert into turno(descricao_turno,numero_turno) values ('returno',2);
